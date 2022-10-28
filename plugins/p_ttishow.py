06c8d9f1e861426e5278f51a069846f146eb7254
Broken Heart <107481248+Kingmaker003@users.noreply.github.com>
2022-10-28 10:50:10 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -22,7 +22,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton('🌐 Support', url=f'https://t.me/MOVIES_ZILAA')
+                InlineKeyboardButton('🌐 Support', url=f'https://t.me/rcmovierequest')
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(
@@ -70,7 +70,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('🌐 Support', url=f'https://t.me/MOVIES_ZILAA')
+            InlineKeyboardButton('🌐 Support', url=f'https://t.me/rcmovierequest')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -109,7 +109,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('🌐 Support', url=f'https://t.me/MOVIES_ZILAA')
+            InlineKeyboardButton('🌐 Support', url=f'https://t.me/rcmovierequest')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
